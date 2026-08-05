e71c8542acbf53fe659ca8f34ef489b4ee3b03a0
che1nov <ilya.chernov1@icloud.com>
2026-08-05 21:51:02 +0400

    style(postgres): normalize error messages

--- backend/internal/postgres/postgres.go
+++ backend/internal/postgres/postgres.go
@@ -13,17 +13,17 @@ func New(ctx context.Context, cfg config.Config) (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(cfg.DSN()), &gorm.Config{})
 
 	if err != nil {
-		return nil, fmt.Errorf("Can't connect to postgress %w", err)
+		return nil, fmt.Errorf("can't connect to postgres: %w", err)
 	}
 
 	sqlDB, err := db.DB()
 	if err != nil {
-		return nil, fmt.Errorf("Can't get database connection: %w", err)
+		return nil, fmt.Errorf("can't get database connection: %w", err)
 	}
 
 	if err := sqlDB.PingContext(ctx); err != nil {
 		_ = sqlDB.Close()
-		return nil, fmt.Errorf("Can't ping PostgreSQL: %w", err)
+		return nil, fmt.Errorf("can't ping postgres: %w", err)
 	}
 
 	return db, nil
