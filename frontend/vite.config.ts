7e89afd6d1a045d819f46fa28d906002ca0ef1cb
Kate Lunkova <e.lunkova@g.nsu.ru>
2026-08-07 23:30:00 +0700

    fix: vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
       '/static': {
         target: 'http://localhost:8081',
         changeOrigin: true,
